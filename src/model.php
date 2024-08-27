ff99ec823e90cb95ef536a5fcdfbf11a05b54d06
leofountas <leofoutas@gmail.com>
2024-08-27 20:03:37 +0200

    added comment features to the blog

--- src/model.php
+++ src/model.php
@@ -33,32 +33,12 @@ function getPost($identifier) {
     	'title' => $row['title'],
     	'french_creation_date' => $row['french_creation_date'],
     	'content' => $row['content'],
+		'identifier' => $row['id'],
 	];
 
 	return $post;
 }
 
-function getComments($identifier)
-{
-	$database = dbConnect();
-	$statement = $database->prepare(
-    	"SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
-	);
-	$statement->execute([$identifier]);
-
-	$comments = [];
-	while (($row = $statement->fetch())) {
-    	$comment = [
-        	'author' => $row['author'],
-        	'french_creation_date' => $row['french_creation_date'],
-        	'comment' => $row['comment'],
-    	];
-
-    	$comments[] = $comment;
-	}
-
-	return $comments;
-}
 
 // Nouvelle fonction qui nous permet d'éviter de répéter du code
 function dbConnect()
