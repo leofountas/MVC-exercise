bb0459c1aa915fc622c680f8d2c99d50504b4f0f
leofountas <leofoutas@gmail.com>
2024-08-27 13:30:37 +0200

    added comments page

--- src/model.php
+++ src/model.php
@@ -1,14 +1,8 @@
 <?php
 
-function getPosts() {
-	// We connect to the database.
-	try {
-    	$database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-	} catch(Exception $e) {
-    	die('Erreur : '.$e->getMessage());
-	}
 
-	// We retrieve the 5 last blog posts.
+function getPosts() {
+	$database = dbConnect();
 	$statement = $database->query(
     	"SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
 	);
@@ -18,10 +12,62 @@ function getPosts() {
         	'title' => $row['title'],
         	'french_creation_date' => $row['french_creation_date'],
         	'content' => $row['content'],
+        	'identifier' => $row['id'],
     	];
 
     	$posts[] = $post;
 	}
 
 	return $posts;
+}
+
+function getPost($identifier) {
+	$database = dbConnect();
+	$statement = $database->prepare(
+    	"SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts WHERE id = ?"
+	);
+	$statement->execute([$identifier]);
+
+	$row = $statement->fetch();
+	$post = [
+    	'title' => $row['title'],
+    	'french_creation_date' => $row['french_creation_date'],
+    	'content' => $row['content'],
+	];
+
+	return $post;
+}
+
+function getComments($identifier)
+{
+	$database = dbConnect();
+	$statement = $database->prepare(
+    	"SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
+	);
+	$statement->execute([$identifier]);
+
+	$comments = [];
+	while (($row = $statement->fetch())) {
+    	$comment = [
+        	'author' => $row['author'],
+        	'french_creation_date' => $row['french_creation_date'],
+        	'comment' => $row['comment'],
+    	];
+
+    	$comments[] = $comment;
+	}
+
+	return $comments;
+}
+
+// Nouvelle fonction qui nous permet d'éviter de répéter du code
+function dbConnect()
+{
+	try {
+    	$database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+
+    	return $database;
+	} catch(Exception $e) {
+    	die('Erreur : '.$e->getMessage());
+	}
 }
